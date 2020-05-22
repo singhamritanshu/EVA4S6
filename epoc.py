b942af1190b663f414275eabebaf411bd9f8ef98
Amritanshu Singh <64220796+singhamritanshu@users.noreply.github.com>
2020-05-22 16:09:17 +0530

    Added the l1 regularization

--- epoc.py
+++ epoc.py
@@ -5,17 +5,31 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 import load_data as a
-import with_bn as c
+import model_arc as c
 from tqdm import tqdm
-import back_prop as b 
-import l1_bn as m 
-l = [c,m]
-for i in l:
-    model = i.Net().to(i.device)
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-    EPOCHS = 3
+import train_test as t
+import l1_reg as l1
+import matplotlib.pyplot as plt
 
+model = c.Net().to(c.device)
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+EPOCHS = 15
+def visualize_graph(train_losses, train_acc, test_losses, test_acc):
+  fig, axs = plt.subplots(2,2,figsize=(15,10))
+  axs[0, 0].plot(train_losses)
+  axs[0, 0].set_title("Training Loss")
+  axs[1, 0].plot(train_acc[4000:])
+  axs[1, 0].set_title("Training Accuracy")
+  axs[0, 1].plot(test_losses)
+  axs[0, 1].set_title("Test Loss")
+  axs[1, 1].plot(test_acc)
+  axs[1, 1].set_title("Test Accuracy")
+l = [t,l1]
+for i in l:
+    print(i)
     for epoch in range(EPOCHS):
         print("EPOCH:", epoch)
-        b.train(model, i.device,a.train_loader, optimizer, epoch)
-        b.test(model, i.device, a.test_loader)
+        i.train(model, c.device,a.train_loader, optimizer, epoch)
+        i.test(model, c.device, a.test_loader)
+
+visualize_graph(i.train_losses, i.train_acc, i.test_losses, i.test_acc)
